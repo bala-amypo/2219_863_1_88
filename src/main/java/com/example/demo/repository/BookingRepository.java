27f5d2ba2b9bab77d6d839553bfec080681f0b7e
bala-amypo <bala@amypo.in>
2025-12-21 10:20:47 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/BookingRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Booking;
+import com.example.demo.model.Facility;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.LocalDateTime;
+
