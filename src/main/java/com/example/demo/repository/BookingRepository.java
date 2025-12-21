798f55e76261b445b6f62608dacdbd9e588d1975
bala-amypo <bala@amypo.in>
2025-12-21 13:02:05 +0000

    User commit

--- src/main/java/com/example/demo/repository/BookingRepository.java
+++ src/main/java/com/example/demo/repository/BookingRepository.java
@@ -4,10 +4,14 @@ import com.example.demo.model.Booking;
 import com.example.demo.model.Facility;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDateTime;   // ✅ CORRECT
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
-    List<Booking> findByFacilityAndStartTimeLessThanAndEndTimeGreaterThan(Facility facility,LocalDateTime start,LocalDateTime end);
+    List<Booking> findByFacilityAndStartTimeLessThanAndEndTimeGreaterThan(
+            Facility facility,
+            LocalDateTime endTime,
+            LocalDateTime startTime
+    );
 }
