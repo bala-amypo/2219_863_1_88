ce98426930c8d89bfeec9a4b95baab8205426518
bala-amypo <bala@amypo.in>
2025-12-21 11:42:40 +0000

    User commit

--- src/main/java/com/example/demo/repository/BookingLogRepository.java
+++ src/main/java/com/example/demo/repository/BookingLogRepository.java
@@ -3,8 +3,10 @@ package com.example.demo.repository;
 import com.example.demo.model.Booking;
 import com.example.demo.model.BookingLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
-public interface BookingLogrepository extends JpaRepository<BookingLog,Long>{
-    List<BookingLog>findByBookingOrderByLoggedAtAsc(Booking booking);
+public interface BookingLogRepository extends JpaRepository<BookingLog, Long> {
+
+    List<BookingLog> findByBookingOrderByLoggedAtAsc(Booking booking);
 }
--- src/main/java/com/example/demo/repository/BookingRepository.java
+++ src/main/java/com/example/demo/repository/BookingRepository.java
@@ -3,9 +3,11 @@ package com.example.demo.repository;
 import com.example.demo.model.Booking;
 import com.example.demo.model.Facility;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.LocalDateTime;
+
+import java.time.LocalDateTime;   // ✅ CORRECT
 import java.util.List;
 
-public interface BookingRepository extends JpaRepository<Booking,Long>{
-    List<Booking>findByFacilityAndStartTimeLessThanEndTimeGreaterThan(Facility facility,LocalDateTime start,LocalDateTime end);
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+    List<Booking> findByFacilityAndStartTimeLessThanAndEndTimeGreaterThan(Facility facility,LocalDateTime start,LocalDateTime end);
 }
