39b1441f4e5078252a362215cb84137d7c012e9d
bala-amypo <bala@amypo.in>
2025-12-26 08:44:04 +0000

    User commit

--- src/main/java/com/example/demo/config/ServletConfig.java
+++ src/main/java/com/example/demo/config/ServletConfig.java
@@ -1,7 +1,16 @@
 package com.example.demo.config;
 
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.example.demo.servlet.SimpleHelloServlet;
 
 @Configuration
 public class ServletConfig {
+
+    @Bean
+    public ServletRegistrationBean<SimpleHelloServlet> helloServlet() {
+        return new ServletRegistrationBean<>(new SimpleHelloServlet(), "/hello-servlet");
+    }
 }
--- src/main/java/com/example/demo/servlet/SimpleHelloServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleHelloServlet.java
@@ -1,26 +1,23 @@
 package com.example.demo.servlet;
 
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/hello-servlet")
 public class SimpleHelloServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException {
-
-        resp.setStatus(HttpServletResponse.SC_OK);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/plain");
+        resp.setStatus(HttpServletResponse.SC_OK);
         resp.getWriter().write("Hello from Simple Servlet");
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException {
-
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setStatus(HttpServletResponse.SC_OK);
     }
 }
