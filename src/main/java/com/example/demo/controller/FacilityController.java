c58fc75f86efa460e392d065cd901d113deb2f26
bala-amypo <bala@amypo.in>
2025-12-27 06:44:50 +0000

    User commit

--- src/main/java/com/example/demo/controller/FacilityController.java
+++ src/main/java/com/example/demo/controller/FacilityController.java
@@ -1,35 +1,4 @@
-/*package com.example.demo.controller;
 
-import com.example.demo.model.Facility;
-import com.example.demo.service.FacilityService;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
-
-@RestController
-@RequestMapping("/facilities")
-@Tag(name = "Facility", description = "Facility management endpoints")
-public class FacilityController {
-    
-    private final FacilityService facilityService;
-    
-    public FacilityController(FacilityService facilityService) {
-        this.facilityService = facilityService;
-    }
-    
-    @PostMapping("/")
-    public ResponseEntity<Facility> addFacility(@RequestBody Facility facility) {
-        Facility added = facilityService.addFacility(facility);
-        return ResponseEntity.ok(added);
-    }
-    
-    @GetMapping("/")
-    public ResponseEntity<List<Facility>> getAllFacilities() {
-        List<Facility> facilities = facilityService.getAllFacilities();
-        return ResponseEntity.ok(facilities);
-    }
-}*/
 
 package com.example.demo.controller;
 
